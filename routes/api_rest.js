5cf7d6e72bc30ef887ac06e41f33304541df8fe4
Chris Suarez <chris.jsa01@gmail.com>
2022-03-21 17:55:25 -0400

    -Remove unused module - QueryParam applied

--- routes/api_rest.js
+++ routes/api_rest.js
@@ -6,6 +6,10 @@ const fs = require("fs");
 
 const secret_url = 'https://echo-serv.tbxnet.com/v1/secret/file'
 
+function hasQueryParams(url) {
+    return url.includes('?');
+}
+
 const isHex = function checkHex(isHex) {
     return /^[0-9A-F]+$/ig.test(isHex);
 }
@@ -16,73 +20,104 @@ const validate_file = function validate(text, number, hex) {
     }
 }
 
-/* GET. */
-router.get('/', async function (req, res) {
-    const file_list = () => {
-        try {
-            return (axios.get(secret_url+'s/', {
-                headers: {
-                    'Authorization': 'Bearer aSuperSecretKey'
-                }
-            }))
-        }
-        catch (error) {
-            console.error(error)
-        }
+const passing_dict = function assign(record) {
+    let current_record = record.split(',')
+    return {
+        'file': current_record[0],
+        'text': current_record[1],
+        'number': current_record[2],
+        'hex': current_record[3]
     }
+}
 
-    // const download_file = (file) => {
-    //     try {
-    //         let url = secret_url+'/'+file
-    //         console.log(url)
-    //         return (axios.get(url, {
-    //             headers: {
-    //                 'Authorization': 'Bearer aSuperSecretKey'
-    //             }
-    //         }))
-    //     }
-    //     catch (error) {
-    //         console.error(error)
-    //     }
-    // }
+const file_list = () => {
+    try {
+        return (axios.get(secret_url+'s/', {
+            headers: {
+                'Authorization': 'Bearer aSuperSecretKey'
+            }
+        }))
+    }
+    catch (error) {
+        console.error(error)
+    }
+}
+
+// const download_file = (file) => {
+//     try {
+//         let url = secret_url+'/'+file
+//         console.log(url)
+//         return (axios.get(url, {
+//             headers: {
+//                 'Authorization': 'Bearer aSuperSecretKey'
+//             }
+//         }))
+//     }
+//     catch (error) {
+//         console.error(error)
+//     }
+// }
+
+/* GET. */
+router.get('', async function (req, res) {
+    const processed_files = []
+    let pass = false
+    let dict_file
 
     // let listed_files = await file_list()
-    let listed_files = [
+    const listed_files = [
         'file1.csv,RgTya,64075909,70ad29aacf0b690b0467fe2b2767f765',
         'file1.csv,AtjW,6,d33a8ca5d36d3106219f66f939774cf5',
         'file1.csv,PNzRfORtKtEDOzmIVrQuSh,74088708,3e29651a63a5202a5661e05a060401fb',
-        'file1.csv,d,6173,f9e1bcdb9e3784acc448af34f47252'
+        'file1.csv,d,6173,f9e1bcdb9e3784acc448af34f47252',
+        'file2.csv,RgTya,998855,70ad29aacf0b690b0467fe2b2767f765'
     ]
 
-    let processed_files = []
-
-    for (const each of listed_files) {
-        let pass = false
-        let filename = 'file1.csv'
-        let current_file = each.split(',')
-        let dict_file = {
-            'file': current_file[0],
-            'text': current_file[1],
-            'number': current_file[2],
-            'hex': current_file[3]
+    if(hasQueryParams(req.url)){
+        if (req.query.fileName) {
+            let file = req.query.fileName.slice(1,-1)
+            if (file.indexOf('.csv') !== -1) {
+                for (let each of listed_files){
+                    if (each.indexOf(file) !== -1) {
+                        dict_file = passing_dict(each)
+                        pass = validate_file(dict_file['text'], dict_file['number'], dict_file['hex'])
+                        if (pass) {
+                            processed_files.push(dict_file)
+                            break
+                        } else {
+                            processed_files.push({'Error':'Formato de archivo no es valido.'})
+                        }
+                    }
+                }
+            } else {
+                processed_files.push({'Error':'Extension de archivo no es valido.'})
+            }
+        } else {
+            processed_files.push({'Error':'Query no es valido.'})
         }
-        if (dict_file['file'] === filename) {
-            pass = validate_file(dict_file['text'], dict_file['number'], dict_file['hex'])
-            if (pass) {
-                processed_files.push(dict_file)
+    }else{
+        for (let each of listed_files) {
+            pass = false
+            let filename = 'file1.csv'
+            let dict_file = passing_dict(each)
+            if (dict_file['file'] === filename) {
+                pass = validate_file(dict_file['text'], dict_file['number'], dict_file['hex'])
+                if (pass) {
+                    processed_files.push(dict_file)
+                }
             }
         }
+
+
+        // for (let each of listed_files.data['files']) {
+        //     let current_file = await download_file(each).then(response => {
+        //         response.data.pipe(fs.createWriteStream('test_file'))
+        //     }).catch(error => {
+        //         console.log(error);
+        //     })
+        //     console.log(current_file)
+        // }
     }
-
-
-    // for (const each of listed_files.data['files']) {
-    //     let current_file = await download_file(each).then(response => {
-    //         response.data.pipe(fs.createWriteStream('test_file'))
-    //     }).catch(error => {
-    //         console.log(error);
-    //     })
-    //     console.log(current_file)
-    // }
 
     res.contentType('application/json')
     res.send(processed_files);
---
+++ test/integration/api_rest.js
@@ -0,0 +1,27 @@
+// test/integration/api_rest.js
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const app = require('../../app');
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+
+describe('API REST controller', () => {
+    it('should get the list of files', (done) => {
+        chai.request(app)
+            .get('/files/data')
+            .end((err, res) => {
+                let data = res.body
+                expect(data).to.be.an('array');
+                for (const each of data) {
+                    expect(each).to.be.have.property('file');
+                    expect(each).to.be.have.property('text');
+                    expect(each).to.be.have.property('number');
+                    expect(each).to.be.have.property('hex');
+                }
+                done();
+            });
+    });
+});
